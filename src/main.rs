666d433fd39ada52e2503461d83351d5ca9e4b5d
Karl Osterberg <karost0249@gmail.com>
2024-05-19 11:35:49 +0200

    Add lunatic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use submillisecond::{router, Application};
+
+fn index() -> &'static str {
+    "Hello :)"
+}
+
+fn main() -> anyhow::Result<()> {
+    Application::new(router! {
+        GET "/" => index
+    })
+    .serve("0.0.0.0:3000")?;
+
+    Ok(())
 }
