ccd57453b1d02a4e97177a8ada1d673383e4a070
Karl Osterberg <karost0249@gmail.com>
2024-05-20 10:25:25 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+
+use lunatic::{Mailbox, MailboxError, Process};
 use serde::{Deserialize, Serialize};
 use submillisecond::{router, static_router, Application};
 use submillisecond_live_view::prelude::*;
@@ -13,13 +16,32 @@ fn main() -> std::io::Result<()> {
 #[derive(Clone, Serialize, Deserialize)]
 struct Counter {
     count: i32,
+    ticker: Option<Process<i32>>,
 }
 
 impl LiveView for Counter {
     type Events = (Increment, Decrement);
 
-    fn mount(_uri: Uri, _socket: Option<Socket>) -> Self {
-        Counter { count: 0 }
+    fn mount(_uri: Uri, socket: Option<Socket>) -> Self {
+        let ticker = match socket.clone() {
+            None => None,
+            Some(socket) => {
+                let ticker =
+                    Process::spawn_link(socket, |mut socket, mailbox: Mailbox<i32>| loop {
+                        match mailbox.receive_timeout(Duration::from_millis(500)) {
+                            Ok(_ms) => (),
+                            Err(MailboxError::TimedOut) => {
+                                socket.send_event(Tick {}).unwrap();
+                            }
+                            err => panic!("{err:?}"),
+                        }
+                    });
+
+                Some(ticker)
+            }
+        };
+
+        Self { count: 0, ticker }
     }
 
     fn render(&self) -> Rendered {
@@ -28,6 +50,15 @@ impl LiveView for Counter {
             button @click=(Decrement) { "Decrement" }
             p { "Count is " (self.count) }
         }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Tick {}
+
+impl LiveViewEvent<Tick> for Counter {
+    fn handle(state: &mut Self, _event: Tick) {
+        state.count += 1;
     }
 }
 
